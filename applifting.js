7e3f6b3f079995fe66efe54da82a57619e7562f4
Damdis <53402332+Damdis@users.noreply.github.com>
2019-07-28 16:56:17 +0200

    Update applifting.js

--- applifting.js
+++ applifting.js
@@ -9,8 +9,15 @@ describe('Applifting - click', () => {
     })
     var i;
     it('Opakované kliknutí', () => {
-    for(i = 0; i < 2500; i++){
+    for(i = 0; i < 9; i++){
         cy.get('.sc-bdVaJa').click() 
     }
+    })
+    it('Opakovaná návštěva stránky', () => {
+        cy.visit('http://test-stfu-applifting.herokuapp.com/')       
+    })
+    it('Pokračování kliknutí', () => {
+        cy.get('#name').type("StarbucksToDokazal")
+        cy.get('.sc-bdVaJa').click()
     })
 })
